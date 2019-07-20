c5bd3f1624ee4f7b8ad2e4ae598f885ff14f70ee
Niklas Grahl <niklas.grahl@gmail.com>
2019-07-20 20:09:35 +0100

    Add prettier and fix help

--- index.js
+++ index.js
@@ -1,88 +1,91 @@
 #!/usr/bin/env node
-const path = require('path');
-const inquirer = require('inquirer');
-const chalk = require('chalk');
-const childProcess = require('child_process');
-const globby = require('globby');
-const pkgUp = require('pkg-up');
-const moveFile = require('./moveFile.js');
+const path = require('path')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const childProcess = require('child_process')
+const globby = require('globby')
+const pkgUp = require('pkg-up')
+const moveFile = require('./moveFile.js')
 
-const run = async ({from, to, move, yes}) => {
+const run = async ({ from, to, move, yes }) => {
   try {
-    const find = new RegExp(from);
+    const find = new RegExp(from)
 
-    const pkg = await pkgUp();
-    const pkgRoot = path.dirname(pkg);
+    const pkg = await pkgUp()
+    const pkgRoot = path.dirname(pkg)
 
-    const allJsFiles = globby.sync('**/*.js', {cwd: pkgRoot, gitignore: true});
+    const allJsFiles = globby.sync('**/*.js', {
+      cwd: pkgRoot,
+      gitignore: true,
+    })
 
     const files = move
       ? allJsFiles
           .filter(file => !!file.match(find))
           .map(file => file.replace('./', ''))
-      : [from];
+      : [from]
 
     if (!files.length) {
-      console.log(chalk.red(`Found no matching files matching ${from}`));
-      process.exit(1);
+      console.log(chalk.red(`Found no matching files matching ${from}`))
+      process.exit(1)
     }
 
-    let fromTo;
+    let fromTo
     if (!to) {
       const maxLength = files.reduce(
         (maxLength, file) => Math.max(maxLength, file.length),
-        0,
-      );
+        0
+      )
       const editText = files
         .map(file => `${file}${' '.repeat(maxLength - file.length)} -> ${file}`)
-        .join('\n');
+        .join('\n')
 
-      const {fromEditor} = await inquirer.prompt([
+      const { fromEditor } = await inquirer.prompt([
         {
           name: 'fromEditor',
           message: `Second argument (to) wasn't specified. Use editor instead?`,
           type: 'editor',
           default: editText,
         },
-      ]);
+      ])
 
       fromTo = fromEditor
         .split('\n')
         .map(line => line.split('->').map(s => s.trim()))
-        .filter(([from, to]) => from && from !== to);
+        .filter(([from, to]) => from && from !== to)
     } else {
-      fromTo = files.map(file => [file, file.replace(find, to)]);
+      fromTo = files.map(file => [file, file.replace(find, to)])
     }
 
-    console.log(chalk.yellow('This will rename the following files:'));
-    console.table(fromTo);
+    console.log(chalk.yellow('This will rename the following files:'))
+    console.table(fromTo)
     console.log(
       chalk.yellow(
-        "⚠️  It's advised NOT to have any untracked changes when running this ⚠️",
-      ),
-    );
+        "⚠️  It's advised NOT to have any untracked changes when running this ⚠️"
+      )
+    )
 
-    const {doRun = yes} = await inquirer.prompt([
-      {name: 'doRun', type: 'confirm', message: 'Continue?', when: !yes},
-    ]);
+    const { doRun = yes } = await inquirer.prompt([
+      { name: 'doRun', type: 'confirm', message: 'Continue?', when: !yes },
+    ])
 
-    if (!doRun) return;
+    if (!doRun) return
     fromTo.forEach(([from, to], index) => {
       console.log(
-        chalk.bgCyan.black(`Processing ${index + 1}/${fromTo.length}`),
-      );
-      moveFile(from, to, pkgRoot, move);
-    });
+        chalk.bgCyan.black(`Processing ${index + 1}/${fromTo.length}`)
+      )
+      moveFile(from, to, pkgRoot, move)
+    })
   } catch (err) {
-    console.error(err);
-    process.exit(1);
+    console.error(err)
+    process.exit(1)
   }
-};
+}
 
-const {argv} = require('yargs')
+const { argv } = require('yargs')
   .command(
     '$0 <from> [to]',
-    'Moves JS file(s) and updates all imports to/from that file(s)',
+    'Moves .js file(s) and updates all relative imports to/from that file(s)',
     yargs =>
       yargs
         .positional('from', {
@@ -91,7 +94,7 @@ const {argv} = require('yargs')
         })
         .positional('to', {
           describe:
-            'target file path (use $1, $2 etc. to reference captured groups)',
+            'target file path (use $1, $2 etc. to reference captured groups). If not specified, opens your default editor',
           type: 'string',
         })
         .option('yes', {
@@ -108,17 +111,21 @@ const {argv} = require('yargs')
         .example('$0 A.js B.js', 'Rename file A.js to B.js')
         .example(
           '$0 src/{A,B}.js',
-          'Rename src/A.js to src/B.js using bash brace expansion',
+          'Rename src/A.js to src/B.js using bash brace expansion'
         )
         .example(
           `$0 '(.*).test.js' '$1.spec.js'`,
-          'Rename all *.test.js files to *.spec.js',
+          'Rename all *.test.js files to *.spec.js'
+        )
+        .example(
+          `$0 'src/(.*).js'`,
+          'Rename all *.js files in the src directory using your default editor'
         )
         .example(
           `$0 '(reducers|actions)/([^/]*).js' '$2/$1.js'`,
-          'Group redux files by domain instead of by type',
-        ),
+          'Group redux files by feature instead of by type'
+        )
   )
-  .help();
+  .help()
 
-run(argv);
+run(argv)
--- moveFile.js
+++ moveFile.js
@@ -1,29 +1,29 @@
-const path = require('path');
-const fs = require('fs');
-const childProcess = require('child_process');
-const globby = require('globby');
+const path = require('path')
+const fs = require('fs')
+const childProcess = require('child_process')
+const globby = require('globby')
 const execSync = cmd =>
   childProcess.execSync(cmd, {
     stdio: ['pipe', process.stdout, process.stderr],
-  });
+  })
 
 module.exports = (from, to, root, move = true) => {
-  from = path.resolve(root, from);
-  to = path.resolve(root, to);
-  const toDir = path.dirname(to);
+  from = path.resolve(root, from)
+  to = path.resolve(root, to)
+  const toDir = path.dirname(to)
   const codemodDir = path.resolve(
     __dirname,
-    'node_modules/refactoring-codemods/lib/transformers',
-  );
+    'node_modules/refactoring-codemods/lib/transformers'
+  )
 
-  fs.mkdirSync(toDir, {recursive: true});
-  if (move) fs.renameSync(from, to);
+  fs.mkdirSync(toDir, { recursive: true })
+  if (move) fs.renameSync(from, to)
 
   const allJsFiles = globby.sync('**/*.js', {
     cwd: root,
     absolute: true,
     gitignore: true,
-  });
+  })
 
   execSync(`
     jscodeshift \
@@ -32,7 +32,7 @@ module.exports = (from, to, root, move = true) => {
       --prevFilePath=${from} \
       --nextFilePath=${to} \
       --parser=flow
-  `);
+  `)
 
   execSync(`
     jscodeshift \
@@ -41,5 +41,5 @@ module.exports = (from, to, root, move = true) => {
       --prevFilePath=${from} \
       --nextFilePath=${to} \
       --parser=flow
-  `);
-};
+  `)
+}
