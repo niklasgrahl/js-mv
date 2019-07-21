085d0f04f11f87707da28cfc1e001ed6d01f9731
Niklas Grahl <niklas.grahl@gmail.com>
2019-07-21 21:33:12 +0100

    Add git mode (--git)

--- index.js
+++ index.js
@@ -7,71 +7,76 @@ const globby = require('globby')
 const pkgUp = require('pkg-up')
 const moveFile = require('./moveFile.js')
 
-const run = async ({ from, to, move, regex, yes }) => {
-  try {
-    const find = regex ? new RegExp(from) : from
+const getRenames = async ({ pkgRoot, regex, from, to, move, git }) => {
+  const find = regex ? new RegExp(from) : from
 
+  if (git) return basedOnGit(pkgRoot)
+
+  const allJsFiles = globby.sync('**/*.js', {
+    cwd: pkgRoot,
+    gitignore: true,
+  })
+
+  const files = move
+    ? allJsFiles
+        .filter(file =>
+          regex
+            ? !!file.match(find)
+            : path.resolve(find) === path.resolve(pkgRoot, file)
+        )
+        .map(file => file.replace('./', ''))
+    : [from]
+
+  if (!files.length) {
+    console.log(
+      chalk.red(
+        `Found no matching files matching ${from}. Did you forget --regex?`
+      )
+    )
+    process.exit(1)
+  }
+
+  if (!to) {
+    const maxLength = files.reduce(
+      (maxLength, file) => Math.max(maxLength, file.length),
+      0
+    )
+    const editText = files
+      .map(file => `${file}${' '.repeat(maxLength - file.length)} -> ${file}`)
+      .join('\n')
+
+    const { fromEditor } = await inquirer.prompt([
+      {
+        name: 'fromEditor',
+        message: `Second argument (to) wasn't specified. Use editor instead?`,
+        type: 'editor',
+        default: editText,
+      },
+    ])
+
+    return fromEditor
+      .split('\n')
+      .map(line => line.split('->').map(s => s.trim()))
+      .filter(([from, to]) => from && from !== to)
+  } else {
+    return files.map(file => [file, file.replace(find, to)])
+  }
+}
+
+const run = async ({ from, to, git, move, regex, yes }) => {
+  try {
     const pkg = await pkgUp()
     const pkgRoot = path.dirname(pkg)
 
-    const allJsFiles = globby.sync('**/*.js', {
-      cwd: pkgRoot,
-      gitignore: true,
-    })
+    const renames = await getRenames({ pkgRoot, from, to, git, move, regex })
 
-    const files = move
-      ? allJsFiles
-          .filter(file =>
-            regex
-              ? !!file.match(find)
-              : path.resolve(find) === path.resolve(pkgRoot, file)
-          )
-          .map(file => file.replace('./', ''))
-      : [from]
-
-    if (!files.length) {
-      console.log(
-        chalk.red(
-          `Found no matching files matching ${from}. Did you forget --regex?`
-        )
-      )
-      process.exit(1)
-    }
-
-    let fromTo
-    if (!to) {
-      const maxLength = files.reduce(
-        (maxLength, file) => Math.max(maxLength, file.length),
-        0
-      )
-      const editText = files
-        .map(file => `${file}${' '.repeat(maxLength - file.length)} -> ${file}`)
-        .join('\n')
-
-      const { fromEditor } = await inquirer.prompt([
-        {
-          name: 'fromEditor',
-          message: `Second argument (to) wasn't specified. Use editor instead?`,
-          type: 'editor',
-          default: editText,
-        },
-      ])
-
-      fromTo = fromEditor
-        .split('\n')
-        .map(line => line.split('->').map(s => s.trim()))
-        .filter(([from, to]) => from && from !== to)
-    } else {
-      fromTo = files.map(file => [file, file.replace(find, to)])
-    }
-
-    if (!fromTo.length) {
+    if (!renames.length) {
       console.log(chalk.red(`Found no files to update`))
       process.exit(1)
     }
 
     console.log(chalk.yellow('This will rename the following files:'))
-    console.table(fromTo)
+    console.table(renames)
     console.log(
       chalk.yellow(
         "⚠️  It's advised NOT to have any untracked changes when running this ⚠️"
@@ -83,9 +88,9 @@ const run = async ({ from, to, move, regex, yes }) => {
     ])
 
     if (!doRun) return
-    fromTo.forEach(([from, to], index) => {
+    renames.forEach(([from, to], index) => {
       console.log(
-        chalk.bgCyan.black(`Processing ${index + 1}/${fromTo.length}`)
+        chalk.bgCyan.black(`Processing ${index + 1}/${renames.length}`)
       )
       moveFile(from, to, pkgRoot, move)
     })
@@ -93,6 +98,25 @@ const run = async ({ from, to, move, regex, yes }) => {
     console.error(err)
     process.exit(1)
   }
+}
+
+const basedOnGit = pkgRoot => {
+  const renames = childProcess
+    .execSync(
+      `git add '${pkgRoot}' && git diff --cached --summary --diff-filter=R`,
+      {
+        encoding: 'utf-8',
+      }
+    )
+    .split('\n')
+    .filter(line => !!line)
+    .map(line => line.replace(/ rename (.*) \(.*%\)/, '$1'))
+    .map(line => line.replace(/(.*)\{(.*) => (.*)\}(.*)/, '$1$2$4 => $1$3$4'))
+    .map(line => line.split(' => '))
+
+  childProcess.execSync(`git reset '${pkgRoot}'`)
+
+  return renames
 }
 
 const { argv } = require('yargs')
@@ -111,6 +135,12 @@ const { argv } = require('yargs')
             'target file path (use $1, $2 etc. to reference captured groups). If not specified, opens your default editor',
           type: 'string',
         })
+        .option('git', {
+          describe:
+            'Detect renamed files with git. <from> needs to be specified but will be ignored',
+          type: 'boolean',
+          default: false,
+        })
         .option('regex', {
           describe: 'Treat <from> as a regex. Uses `new RegExp(from)`',
           type: 'boolean',
@@ -128,6 +158,10 @@ const { argv } = require('yargs')
           default: true,
         })
         .example('$0 A.js B.js', 'Rename file A.js to B.js')
+        .example(
+          '$0 . --git',
+          'Find and update imports in renamed files using git'
+        )
         .example(
           '$0 src/{A,B}.js',
           'Rename src/A.js to src/B.js using bash brace expansion'
